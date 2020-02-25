7c43c1542db181d6b9c56642f4beba043da97f47
Ezequiel Gomes <ezekigomes775@gmail.com>
2020-02-25 12:34:38 -0300

    definição da velocidade e posição de inicio da Snake

--- tela_exibicao.py
+++ tela_exibicao.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 branco = (255, 255, 255)
 preto = (0, 0, 0)
@@ -13,9 +14,11 @@ except print:
 
 largura = 640
 altura = 480
-posicao_x = largura/2
-posicao_y = altura/2
 tamanho = 10
+posicao_x = randint(0, (largura - tamanho)/10)*10
+posicao_y = randint(0, (altura - tamanho)/10)*10
+velocidade_x = 0
+velocidade_y = 0
 
 fundo = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Snake")
@@ -27,9 +30,23 @@ while sair:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sair = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                velocidade_y = 0
+                velocidade_x = - 0.2
+            if event.key == pygame.K_RIGHT:
+                velocidade_y = 0
+                velocidade_x = 0.2
+            if event.key == pygame.K_UP:
+                velocidade_x = 0
+                velocidade_y = - 0.2
+            if event.key == pygame.K_DOWN:
+                velocidade_x = 0
+                velocidade_y = 0.2
     fundo.fill(branco)
     pygame.draw.rect(fundo, preto, [posicao_x, posicao_y, tamanho, tamanho])
-
+    posicao_x += velocidade_x
+    posicao_y += velocidade_y
     # print(event)
     """/impressao dos eventos que acontece
         no jogo"""
